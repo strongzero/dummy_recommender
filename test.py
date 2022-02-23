947366da687204f4cb6c978baeadf31e7e717481
Shohei Suzuki <40532590+strongzero@users.noreply.github.com>
2022-02-23 15:38:17 +0100

    Create test.py

---
+++ test.py
@@ -0,0 +1,12 @@
+"module containing all tests on my recommender"
+
+import pandas as pd
+from recommender import recommend_random
+
+# test recommend_random function
+def test_recommender():
+    "testing that my recommender returns the default k recommendations"
+    movies = pd.read_csv('data/movies.csv')
+    recommendations = recommend_random(movies=movies)
+    assert len(recommendations)==10
+    
